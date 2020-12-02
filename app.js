d3149deb328d7a46baf8ce95b2e526efab99feb8
1805ayush <ayush.chakladar@gmail.com>
2020-12-03 01:35:38 +0530

    add env files

--- app.js
+++ app.js
@@ -23,9 +23,9 @@ const userSchema = new mongoose.Schema({
     password: String,
 });
 
-const secret = "Thisisalittlelongsecret";
 
-userSchema.plugin(encrypt,{secret: secret, encryptedFields: ['password']}); 
+
+userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields: ['password']}); 
 
 const User = new mongoose.model("User",userSchema);
 
